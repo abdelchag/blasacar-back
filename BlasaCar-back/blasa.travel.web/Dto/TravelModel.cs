d3f771d950c2c0d1e7dcea98744b275a2c3d0d82
amine barrima <amine.barrima@s2hgroup.com>
2021-03-26 01:27:39 +0100

    commit travel model

--- BlasaCar-back/blasa.travel.web/Dto/TravelModel.cs
+++ BlasaCar-back/blasa.travel.web/Dto/TravelModel.cs
@@ -2,22 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace blasa.travel.web.Models
 {
     public class TravelModel
     {
         public int Id{ get; set; }
+        [Required(ErrorMessage = "DepartureCity is required")]
         public string DepartureCity { get; set; }
-
+        [Required(ErrorMessage = "ArrivalCity is required")]
         public string ArrivalCity { get; set; }
+
+        [Required(ErrorMessage = "DepartureDate is required")]
         public DateTime DepartureDate { get; set; }
 
         //we can use also TimeSpan as type for DepartureTime
+        [Required(ErrorMessage = "DepartureTime is required")]
         public DateTime DepartureTime { get; set; }
+        [Required(ErrorMessage = "NumberPlaces is required")]
         public int NumberPlaces { get; set; }
+        [Required(ErrorMessage = "IsAutomaticAcceptance is required")]
         public bool IsAutomaticAcceptance { get; set; }
+        [Required(ErrorMessage = "Price is required")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "PhoneNumber is required")]
         public string PhoneNumber { get; set; }
     }
 }
