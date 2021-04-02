2d85c773a095443ed9db6b87f92aa14a5ea995c7
amine barrima <amine.barrima@s2hgroup.com>
2021-04-02 17:25:28 +0200

    travel

---
+++ BlasaCar-back/blasa.travel.core/Application/InputPort/ITravelCommandAsync.cs
@@ -0,0 +1,12 @@
+﻿using blasa.travel.Core.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace blasa.travel.Core.Application.Commands
+{
+    public interface ITravelCommandAsync
+    {
+        Task<Travel> GetTravelByUserEmailAsync(string UserEmail);
+        Task<Travel> GetTravelByUserIdAsync(Guid id);
+    }
+}
