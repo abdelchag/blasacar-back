fc79bf32fef590436e497d4ac37c0aeeed053d1a
Misbahi Mohamed <misbahi.med@gmail.com>
2021-04-19 19:49:15 +0200

    filtre travel back

--- BlasaCar-back/blasa.travel.persistance/RepositoriesAdapter/TravelRepositoryAsync.cs
+++ BlasaCar-back/blasa.travel.persistance/RepositoriesAdapter/TravelRepositoryAsync.cs
@@ -26,7 +26,22 @@ namespace blasa.travel.persistance.Repositories
             return await _dbContext.Travels.Where(t => t.Userid == id).OrderByDescending(t => t.DepartureDate).ToListAsync();
 
         }
+        public async Task<IReadOnlyList<Travel>> GetTravelByFiltre(Travel filreTravel)
+        {
+            var list = _dbContext.Travels.Where(t =>
+            t.Userid == filreTravel.Userid || string.IsNullOrWhiteSpace(filreTravel.Userid) &&
+            t.ArrivalCity == filreTravel.ArrivalCity || string.IsNullOrWhiteSpace(filreTravel.ArrivalCity) &&
+            t.DepartureCity == filreTravel.DepartureCity || string.IsNullOrWhiteSpace(filreTravel.DepartureCity) &&
+            t.DepartureDate == filreTravel.DepartureDate || filreTravel.DepartureDate == null &&
+            t.DepartureTime == filreTravel.DepartureTime || filreTravel.DepartureTime == null &&
+            t.IsAutomaticAcceptance == filreTravel.IsAutomaticAcceptance || filreTravel.IsAutomaticAcceptance == null &&
+            t.NumberPlaces == filreTravel.NumberPlaces || filreTravel.NumberPlaces == null &&
+            t.PhoneNumber == filreTravel.PhoneNumber || string.IsNullOrWhiteSpace(filreTravel.PhoneNumber) &&
+            t.Price == filreTravel.Price || filreTravel.Price == null);
 
-       
+            return await list.OrderByDescending(t => t.DepartureDate).ToListAsync();
+        }
+
+
     }
 }
--- BlasaCar-back/blasa.travel.web/Dto/FiltreTravelDTO.cs
+++ BlasaCar-back/blasa.travel.web/Dto/FiltreTravelDTO.cs
@@ -1,26 +1,22 @@
-﻿using blasa.access.management.Core.Domain.Entities;
-using blasa.travel.Core.Domain.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace blasa.travel.Core.Domain.Entities
+namespace blasa.travel.web.Models
 {
-   public class Travel : AuditableBaseEntity
+    public class FiltreTravelDTO
     {
-
         public string DepartureCity { get; set; }
-
         public string ArrivalCity { get; set; }
+
         public DateTime? DepartureDate { get; set; }
 
-        //we can use also TimeSpan as type for DepartureTime
         public DateTime? DepartureTime { get; set; }
         public int? NumberPlaces { get; set; }
         public bool? IsAutomaticAcceptance { get; set; }
         public decimal? Price { get; set; }
         public string PhoneNumber { get; set; }
-        public string Userid         { get; set; }
 
     }
 }
